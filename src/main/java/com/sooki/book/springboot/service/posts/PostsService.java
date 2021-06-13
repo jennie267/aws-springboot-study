36986d39f13dbabbebcb7a952a8ba27661f72d66
sooki <jennieee267@gmail.com>
2021-06-13 23:12:52 +0900

    챕터 3.3 - insert

---
+++ src/main/java/com/sooki/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.sooki.book.springboot.service.posts;
+
+import com.sooki.book.springboot.domain.posts.PostsRepository;
+import com.sooki.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
