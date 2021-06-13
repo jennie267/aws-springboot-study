8d44e4fc6e3f9b5eb03241e3962a0822ae7b63fe
sooki <jennieee267@gmail.com>
2021-06-13 23:33:16 +0900

    챕터 3.3 - update

--- src/main/java/com/sooki/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/sooki/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.sooki.book.springboot.service.posts;
 
+import com.sooki.book.springboot.domain.posts.Posts;
 import com.sooki.book.springboot.domain.posts.PostsRepository;
+import com.sooki.book.springboot.web.dto.PostsResponseDto;
 import com.sooki.book.springboot.web.dto.PostsSaveRequestDto;
+import com.sooki.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,4 +19,22 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" +id));
+
+        return new PostsResponseDto(entity);
+    }
 }
+
